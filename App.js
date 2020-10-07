33bfa4e37727d5c2e4a835eafe7f16fcf5b292e0
Ari Steinberg <arimsteinberg@gmail.com>
2020-10-07 13:02:47 -0700

    move PersonList to a separate file

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, Text, TouchableOpacity, View, FlatList, AsyncStorage } from 'react-native';
+import { AsyncStorage } from 'react-native';
 import * as Contacts from 'expo-contacts';
 
 import Contact from './contacts';
 import Person from './Person';
+import PersonList, { ContactsContext } from './PersonList';
 
 const PA_AREA_CODES = [215, 223, 267, 272, 412, 445, 484, 570, 582, 610, 717, 724, 814, 878]
 const PA_NUM_REGEX = '^\\+?1?(' + PA_AREA_CODES.join('|') + ')';
 
 const Stack = createStackNavigator();
-const ContactsContext = React.createContext();
 
 export default function App() {
   const [contacts, _setContacts] = useState([]);
@@ -83,40 +82,6 @@ export default function App() {
 
 }
 
-function PersonList() {
-  return (
-    <ContactsContext.Consumer>{({
-        contacts, importContacts, setContacts
-      }) => (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.headerText}>
-            Your contacts
-          </Text>
-          <TouchableOpacity
-            onPress={importContacts}
-            style={{ backgroundColor: 'blue', padding: 10, borderRadius: 5, marginHorizontal: 5 }}>
-            <Text style={{ fontSize: 20, color: '#fff' }}>Import</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => setContacts([])}
-            style={{ backgroundColor: 'grey', padding: 10, borderRadius: 5, marginHorizontal: 5 }}>
-            <Text style={{ fontSize: 20, color: '#fff' }}>Clear</Text>
-          </TouchableOpacity>
-        </View>
-        <FlatList style={styles.list}
-                  data={contacts}
-                  renderItem={({item}) => <Person contact={item} />}
-                  keyExtractor={(item) => item.id}
-        />
-        <StatusBar style="auto" />
-      </View>
-      )}
-    </ContactsContext.Consumer>
-  );
-
-}
-
 function PersonDetails({route}) {
   const contact = Contact.deserialize(route.params.contact);
   return (
@@ -124,22 +89,3 @@ function PersonDetails({route}) {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 10,
-  },
-  headerText: {
-    color: '#888',
-    fontSize: 24,
-    marginHorizontal: 15,
-  },
-  list: {
-  },
-});
--- Person.js
+++ Person.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View, TouchableWithoutFeedback } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
