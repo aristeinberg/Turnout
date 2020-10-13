c7dd5046bbbd4beca9545eed9fa990829d196190
Ari Steinberg <arimsteinberg@gmail.com>
2020-10-13 01:27:40 -0700

    fix reachout messages

--- pages/ReachOut.js
+++ pages/ReachOut.js
@@ -8,37 +8,38 @@ export default function ReachOut({route}) {
   const { contacts } = useContext(ContactsContext);
   const contact = contacts[route.params.contactId];
 
-  let keyMessage = '';
+  let keyMessage = "Hi, I'm really concerned about this election so I am checking the vote status of all my friends in PA through an app called Turnout.\n\n";
   switch (contact.data.voteStatus) {
-    case VOTE_STATUSES.VBM_REQUESTED:
-      keyMessage = "I heard you requested a mail ballot, keep your eyes out for it! And don't forget the privacy envelope.";
+    case 'VBM_REQUESTED':
+      keyMessage += "I see you requested a mail ballot, keep your eyes out for it! And don't forget the privacy envelope.";
       break;
-    case VOTE_STATUSES.VBM_SENT_BY_OFFICE:
-      keyMessage = "It looks like your mail ballot was sent over to you - did you receive it yet?"
+    case 'VBM_SENT_BY_OFFICE':
+      keyMessage += "It looks like your mail ballot was sent over to you - did you receive it yet?"
       break;
-    case VOTE_STATUSES.VBM_RECEIVED_BY_VOTER:
-      keyMessage = "I know you got that mail ballot. Did you have a chance to send it back in yet?"
+    case 'VBM_RECEIVED_BY_VOTER':
+      keyMessage += "I know you got that mail ballot. Did you have a chance to send it back in yet?"
       break;
-    case VOTE_STATUSES.VBM_SENT_BACK:
-      keyMessage = "It looks like the office still hasn't received your mail ballot. Are you SURE you sent it?"
+    case 'VBM_SENT_BACK':
+      keyMessage += "It looks like the office still hasn't received your mail ballot. Are you SURE you sent it?"
       break;
-    case VOTE_STATUSES.VBM_RECEIVED_BY_OFFICE:
-      keyMessage = "Great job getting your mail ballot back. Did you remember the privacy envelope?"
+    case 'VBM_RECEIVED_BY_OFFICE':
+      keyMessage += "Great job getting your mail ballot back. Did you remember the privacy envelope?"
       break;
-    case VOTE_STATUSES.PLANS_TO_VOTE:
-      keyMessage = "Still on track to vote? Are you going to vote early?"
+    case 'PLANS_TO_VOTE':
+      keyMessage += "Still on track to vote? Are you going to vote early?"
       break;
-    case VOTE_STATUSES.VOTED_BUT_NOT_COUNTED:
-      keyMessage = "Thanks for voting! For some reason the web site still indicates that your vote has not counted. Do you need help figuring this out? Maybe consider a provisional ballot?"
+    case 'VOTED_BUT_NOT_COUNTED':
+      keyMessage += "Thanks for voting! For some reason the web site still indicates that your vote has not counted. Do you need help figuring this out? Maybe consider a provisional ballot?"
       break;
-    case VOTE_STATUSES.VOTE_COUNTED:
-      keyMessage = "Great job voting! Are you open to helping me try to get other people to turn out?"
+    case 'VOTE_COUNTED':
+      keyMessage += "Great job voting! Are you open to helping me try to get other people to turn out?"
       break;
-    case VOTE_STATUSES.UNKNOWN:
+    case 'UNKNOWN':
     default:
-      keyMessage = "Hey, are you planning to vote in this election? You should do it ASAP!";
+      keyMessage += "Are you planning to vote in this election? You should do it ASAP!";
       break;
   }
+  console.log('reachout:', contact.data.voteStatus, keyMessage);
 
   function text() {
     Communications.text(contact.data.phone, keyMessage)
